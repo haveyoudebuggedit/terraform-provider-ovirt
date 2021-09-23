1f8341132db4858cebb22510b7aacf352d676540
Janos <86970079+janosdebugs@users.noreply.github.com>
2021-09-23 11:28:44 +0200

    Better provider description

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,10 +1,21 @@
 provider "ovirt" {
+  # Set this to your oVirt Engine URL, e.g. https://example.com/ovirt-engine/
   url           = var.url
+  # Set this to your oVirt username, e.g. admin@internal
   username      = var.username
+  # Set this to your oVirt password.
   password      = var.password
-  tls_insecure  = var.tls_insecure
-  tls_system    = var.tls_system
+  # Take trusted certificates from the specified files (list).
   tls_ca_files  = var.tls_ca_files
+  # Take trusted certificates from the specified directories (list).
   tls_ca_dirs   = var.tls_ca_dirs
+  # Take the trusted certificates from the provided variable. Certificates must be in PEM format.
   tls_ca_bundle = var.tls_ca_bundle
+  # Set this to true to use the system certificate storage to verify the engine certificate. You must
+  # add the certificate to your trusted roots before running. This option doesn't work on Windows.
+  tls_system    = var.tls_system
+  # Set this to true to disable certificate verification. This is a terrible idea.
+  tls_insecure  = var.tls_insecure
+  # Set to true if you want to run an in-memory test. In this mode all other options will be ignored.
+  mock          = false
 }
